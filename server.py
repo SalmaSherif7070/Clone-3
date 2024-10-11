e27be79ad1d85aae494c72aab6b95b984492c7f6
SalmaSherif7070 <s-salma.ali@zewailcity.edu.eg>
2024-10-11 19:45:52 +0300

    3rd commit

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def form():
+    return render_template("form.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form["name"]
+    email = request.form["email"]
+    return render_template("result.html", name=name, email=email)
+
+if __name__ == "__main__":
+    app.run(debug=True)
